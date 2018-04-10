8d1219a573d5fb21b6bc1a34a5bcd9100c713d1d
Ivan Melnikov <ivanempire@gmail.com>
2018-04-10 12:08:08 -0400

    More comments

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path	  = require("path");
 const app     = express();
 
+// Made this a constant for ease of use
 const WEB_PORT = 3000
 
+// Since you are serving static content - you should all this
 app.use(express.static("view"));
 
 app.get("/", (req, res) => {
@@ -12,5 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(WEB_PORT, () => {
+	// Note the quotation marks - it's a template string literal in JavaScript
+	// You can do stuff like ${2+2} and it will evaluate the expressions
 	console.log(`WebApp listening on port ${WEB_PORT}`);
 });
