df0a8a5fc15736957bfa1d0d1a45258014803f90
Leo Gonsalves <lcgonsalves@wpi.edu>
2018-04-13 13:45:43 -0400

    Trianglify background implemented. Need to fix performance

--- view/script.js
+++ view/script.js
@@ -1,26 +1,45 @@
-/*
-### EXAMPLE CODE FOR VUE.JS ###
-var myModel = {
-  name: "Ashley",
-  age: 24,
-  friends: [
-    { name: "Bob", age: 21 },
-    { name: "Jane", age: 20 },
-    { name: "Anna", age: 29 }
-  ],
-  search: ""
-};
+var color = [2, 185 ,115];
+var forward = true; 
 
-var myViewModel = new Vue({
-  el: '#my_view',
-  data: myModel
-});
-*/
-
+window.setInterval(function() {
 var pattern = Trianglify({
 	height: window.innerHeight,
 	width: window.innerWidth,
-	cell_size: 40
+	cell_size: 40,
+  seed: 123,
+  x_colors: ['#000000', rgbToHex(color[0], color[1], color[2]), '#FFFFFF']
 });
 
 document.body.appendChild(pattern.canvas());
+
+pickColor();
+
+}, 10);
+
+// Methods: converting RGB to HEX
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) 
+               + componentToHex(g) 
+               + componentToHex(b);}
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;}
+
+// Method: algorithm for choosng color
+function pickColor(){
+  if(forward){
+    if(color[2] < color[1])
+      color[2] += 5;
+    else
+      color[1] -= 5;
+
+  } else {
+    if(color[1] < color[2])
+      color[1] += 5;
+    else
+      color[2] -= 5;
+  }
+
+  // check whether fringe values have been met
+  if (color[1] == 115) forward = false;
+  if (color[2] == 115) forward = true;}
